4c56818677c6a23eedaf6d2a89a5009e4addb018
Tahinli <Kamuflaj@protonmail.com>
2024-10-17 23:03:36 +0300

    pre commit

--- artificial_bee_colony/src/bee.rs
+++ artificial_bee_colony/src/bee.rs
@@ -11,32 +11,13 @@ impl Bee {
         upper_bound: f64,
         lower_bound: f64,
     ) {
-        let mut different_food_source_index = rand::thread_rng().gen_range(0..food_sources.len());
-        while different_food_source_index == index {
-            different_food_source_index = rand::thread_rng().gen_range(0..food_sources.len());
-        }
-        let selected_coordinate_index = rand::thread_rng().gen_range(0..decision_variable_count);
-        let randomness = rand::thread_rng().gen_range(-1.0..=1.0);
-
-        let mut candidate_one_index = food_sources[index].coordinates[selected_coordinate_index]
-            + randomness
-                * (food_sources[index].coordinates[selected_coordinate_index]
-                    - food_sources[different_food_source_index].coordinates
-                        [selected_coordinate_index]);
-        if candidate_one_index > upper_bound {
-            candidate_one_index = upper_bound;
-        }
-        if candidate_one_index < lower_bound {
-            candidate_one_index = lower_bound;
-        }
-        let mut candidate_coordinates = food_sources[index].coordinates.clone();
-        candidate_coordinates[selected_coordinate_index] = candidate_one_index;
-        let candidate = FoodSource::new(candidate_coordinates);
-        food_sources[index].try_counter += 1;
-        if candidate.fitness_calculation > food_sources[index].fitness_calculation {
-            food_sources[index] = candidate;
-            food_sources[index].try_counter = 0;
-        }
+        send_bee(
+            food_sources,
+            index,
+            decision_variable_count,
+            upper_bound,
+            lower_bound,
+        );
     }
 
     pub fn onlooker_bee(
@@ -49,7 +30,7 @@ impl Bee {
     ) {
         let fitness_for_index = food_sources[index].fitness_calculation;
         if fitness_for_index / total_fitness <= rand::thread_rng().gen_range(0.0..=1.0) {
-            Self::employed_bee(
+            send_bee(
                 food_sources,
                 index,
                 decision_variable_count,
@@ -77,5 +58,39 @@ impl Bee {
             let new_food_source = FoodSource::new(coordinates_for_new);
             food_sources[most_tried_index] = new_food_source;
         }
+    }
+}
+
+fn send_bee(
+    food_sources: &mut [FoodSource],
+    index: usize,
+    decision_variable_count: usize,
+    upper_bound: f64,
+    lower_bound: f64,
+) {
+    let mut different_food_source_index = rand::thread_rng().gen_range(0..food_sources.len());
+    while different_food_source_index == index {
+        different_food_source_index = rand::thread_rng().gen_range(0..food_sources.len());
+    }
+    let selected_coordinate_index = rand::thread_rng().gen_range(0..decision_variable_count);
+    let randomness = rand::thread_rng().gen_range(-1.0..=1.0);
+
+    let mut candidate_one_index = food_sources[index].coordinates[selected_coordinate_index]
+        + randomness
+            * (food_sources[index].coordinates[selected_coordinate_index]
+                - food_sources[different_food_source_index].coordinates[selected_coordinate_index]);
+    if candidate_one_index > upper_bound {
+        candidate_one_index = upper_bound;
+    }
+    if candidate_one_index < lower_bound {
+        candidate_one_index = lower_bound;
+    }
+    let mut candidate_coordinates = food_sources[index].coordinates.clone();
+    candidate_coordinates[selected_coordinate_index] = candidate_one_index;
+    let candidate = FoodSource::new(candidate_coordinates);
+    food_sources[index].try_counter += 1;
+    if candidate.fitness_calculation > food_sources[index].fitness_calculation {
+        food_sources[index] = candidate;
+        food_sources[index].try_counter = 0;
     }
 }
